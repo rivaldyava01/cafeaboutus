01f291fa45268ac9dc49c2d1218d8f02e7f6858d
rivaldyava <rivaldy.avianto01@gmail.com>
2022-03-19 16:07:23 +0700

    edit host1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'),app = express(),mysql = require('mysql'),cors= require('cors') ,bodyParser = require('body-parser')
 
 db = mysql.createConnection({
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'tugas_soa'
